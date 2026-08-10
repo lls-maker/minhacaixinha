573ddae1be527ed8d4fad2d5192ff015b3bfd5fe
lls-maker <desenvolvedorleles@gmail.com>
2026-08-10 11:50:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
-const CACHE_NAME = 'caixinha-v1';
+const CACHE_NAME = 'caixinha-v2';
 const ASSETS = [
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'logo.ico'
 ];
 
-// Instala o app no armazenamento do celular/computador
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
     );
 });
 
-// Permite que o app abra mesmo se o usuário estiver totalmente sem internet
 self.addEventListener('fetch', e => {
     e.respondWith(
         caches.match(e.request).then(response => response || fetch(e.request))
